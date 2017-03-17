9e2cbff2955ec4cc7b4dd85145382f4b6c4ad852
kamranghiasvand <kamran.ghiasvand@gmail.com>
2017-03-17 23:14:18 +0330

    Add project files.

---
+++ FingerPrintDetectionWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FingerPrintDetectionWeb.Startup))]
+namespace FingerPrintDetectionWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
